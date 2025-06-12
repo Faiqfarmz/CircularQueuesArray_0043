7300ee3177d0cf0bbf915c05ce77290fde9baed0
Faiqfarmz <faiqfarmz@gmail.com>
2025-06-12 10:19:28 +0700

    mengecek antrian

--- Queues.cpp
+++ Queues.cpp
@@ -29,9 +29,18 @@ public:
     if (FRONT == -1) {
         FRONT = 0;
         REAR = 0;
-    else
-        REAR = REAR + 1;
-    }
-    queue_array[REAR] = num;
     
     }
+    else{
+        if (REAR == max - 1)
+            REAR = 0;
+        else 
+            REAR = REAR + 1;
+
+    }
+    queue_array[REAR] = num;
+        
+    }
+    
+    
+}
