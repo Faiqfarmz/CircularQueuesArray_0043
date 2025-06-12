583d84361554e9c762fb3c12f3431bc88d001291
Faiqfarmz <faiqfarmz@gmail.com>
2025-06-12 10:20:32 +0700

    void remove

--- Queues.cpp
+++ Queues.cpp
@@ -41,6 +41,14 @@ public:
     queue_array[REAR] = num;
         
     }
+
+       void remove() {
+        // cek apakah antrian kosong
+        if (FRONT == -1) {
+            cout << "Queue underflow\n"
+            return;
+        }
+    }
     
     
 }
